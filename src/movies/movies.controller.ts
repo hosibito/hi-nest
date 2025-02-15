5bfc8f2072eff335184cd117fd8ef83e88a8a32f
hosibito <hosibito@naver.com>
2025-02-16 00:04:09 +0900

    2.4-5 DTOs and Validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,10 +6,11 @@ import {
     Delete,
     Patch,
     Body,
-    Query
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -19,28 +20,24 @@ export class MoviesController {
     getAll(): Movie[] {
         return this.moviesService.getAll();
     }
-    @Get('search')
-    search(@Query('year') seachingYear: string) {
-        return `We are searching for a movie made after: ${seachingYear}`;
-    }
 
     @Get(':id')
-    getOne(@Param('id') movieId: string): Movie {
+    getOne(@Param('id') movieId: number): Movie {
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(':id')
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId: string, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updateData);
     }
 }
